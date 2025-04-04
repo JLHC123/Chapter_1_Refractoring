265ec36d41b34715a7ce9ae9bdfb64eaa8375a09
Jorge Hernandez Cruz <jlhc3751@gmail.com>
2025-04-04 14:23:50 -0700

    made a new function of calculateTotalAmount, renamed a lot of things to result within the functions.

--- statement.js
+++ statement.js
@@ -3,28 +3,36 @@ let plays = require("./plays.json")
 let invoice = require("./invoice.json") 
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    
     for (let perf of invoice.performances) {
       // print line for this order
       result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
         perf.audience
       } seats)\n`;
-      totalAmount += amountFor(perf);
     }
+
+    let totalAmount = calculateTotalAmount();
+
     let volumeCredits = totalVolumeCredits();
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(calculateTotalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
   }
 
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
+  function calculateTotalAmount() {
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);      
+      result += amountFor(perf);
     }
-    return volumeCredits;
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);      
+    }
+    return result;
   }
 
   function usd(aNumber) {
